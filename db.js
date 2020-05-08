841154cd13764fd6aa2b4541780cce77070f3dbf
Mariam Magdy <magdymariam@gmail.com>
2020-05-09 01:15:44 +0200

    remove Db URL

--- db.js
+++ db.js
@@ -1,13 +1,10 @@
 const mongoose = require("mongoose");
 mongoose.set("useCreateIndex", true);
 mongoose
-  .connect(
-    "mongodb+srv://Mariam:Mariam@cluster0-6dg7e.mongodb.net/test?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    }
-  )
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => {
     console.log("sucess");
   })
